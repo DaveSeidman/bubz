5e7558a99dfc027e9d97b2b30d08e95d4ca69afc
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2024-11-12 08:01:17 -0500

    testing bubbles

--- src/Scene/index.jsx
+++ src/Scene/index.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useMemo, useState, useRef } from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { PerspectiveCamera, Environment } from '@react-three/drei';
-import * as THREE from 'three';
+import { Shape, Color } from 'three';
 import './index.scss';
 
 function ExtrudedShape({ points, width, height, age }) {
   const shape = useMemo(() => {
-    const newShape = new THREE.Shape();
+    const newShape = new Shape();
     newShape.moveTo((points[0].x / (width / 2)) - 1, (points[0].y / (height / -2)) + 1);
     points.slice(1).forEach((point) => {
       newShape.lineTo((point.x / (width / 2)) - 1, (point.y / (height / -2)) + 1);
@@ -37,7 +37,7 @@ function ExtrudedShape({ points, width, height, age }) {
         reflectivity={1}
         envMapIntensity={3}
         ior={1.33}
-        attenuationTint={new THREE.Color(0.2, 0.6, 1)}
+        attenuationTint={new Color(0.2, 0.6, 1)}
         attenuationDistance={0.1}
       />
     </mesh>
@@ -45,11 +45,21 @@ function ExtrudedShape({ points, width, height, age }) {
 }
 
 function Bubble({ bubble }) {
+  const [position, setPosition] = useState(bubble.position);
   useFrame(() => {
-    // move bubble on z axis while it's attached property is true
-    // move bubble on y axis when it's attached property is false
-    // if it's y position is greater than 1, remove it from the bubble array
+    setPosition((prevPosition) => [prevPosition[0], prevPosition[1] + bubble.attached ? 0 : 0.01, prevPosition[2] + 0.1]);
   });
+
+  return (
+    <mesh
+      position={position}
+    >
+      <sphereGeometry
+        args={[0.01, 8, 16]}
+      />
+      <meshNormalMaterial />
+    </mesh>
+  );
 }
 
 export default function Scene({ loops, width, height }) {
@@ -57,9 +67,28 @@ export default function Scene({ loops, width, height }) {
   const prevLoops = useRef([]);
 
   useEffect(() => {
-    // Check for new loops and lost loops and update bubbles accordingly
-    // new loops should add a bubble to the bubble array
-    // lost loops should update that bubble's attached property to false
+    const loopsAdded = loops.filter((loop) => !prevLoops.current.includes(loop.id));
+    const loopsRemoved = prevLoops.current.filter((loop) => !loops.includes(loop.id));
+
+    setBubbles((prevBubbles) => {
+      const nextBubbles = [...prevBubbles];
+      // add new bubbles
+      loopsAdded.forEach((loop) => {
+        nextBubbles.push({
+          id: loop.id,
+          center: loop.center, // TODO: add a random offset
+          position: [loop.center.x, loop.center.y, 0],
+          velocity: { x: 0, y: 0, z: 0.1 },
+          attached: true,
+        });
+      });
+      // detach bubbles if loop disappeared
+      nextBubbles.forEach((bubble) => {
+        if (loopsRemoved.some((loop) => loop.id === bubble.id)) bubble.attached = false;
+      });
+      return nextBubbles;
+    });
+
     prevLoops.current = loops;
   }, [loops]);
 
